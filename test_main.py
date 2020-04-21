6fec40f204c59dea219c3dd39d372c42225cd5c7
RUSHIL KAUSHAL SANGHAVI <50961163+Rushil231100@users.noreply.github.com>
2020-04-21 19:36:59 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,5 +2,6 @@ import main
 def test_multiply():
   result = main.multiply(3, 4)
   result_2 = main.multiply(2, 3)
-  assert result_2 = 4
+  print("running tests..")
+  assert result_2 == 4
   assert result == 12
